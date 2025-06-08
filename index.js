449cde9e49aa70f3d862ef93d386d16f1e23ab84
Rdj23 <rohan.jadhav@clevertap.com>
2025-06-08 14:08:33 +0530

    Describe your changes here

--- index.js
+++ index.js
@@ -2,16 +2,17 @@
 // importing any libraries that expect it to be set up.
 import 'react-native/Libraries/Core/InitializeCore';
 
+import ErrorUtilsVendor from 'react-native/Libraries/vendor/core/ErrorUtils';
+
 import '@react-native/js-polyfills/error-guard';
 
 if (!global.ErrorUtils) {
-    // eslint-disable-next-line no-undef
-    global.ErrorUtils = require('react-native/Libraries/vendor/core/ErrorUtils');
+   global.ErrorUtils = ErrorUtilsVendor;
 } else if (typeof global.ErrorUtils.reportFatalError !== 'function') {
   global.ErrorUtils.reportFatalError = error => {
     console.error('[reportFatalError]', error);
-  }
-};
+  };
+}
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App.js';
